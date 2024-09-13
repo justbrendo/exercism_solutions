e7f66f5abefbe17b2f1314403f8a86747a4fc5eb
Brendo <brendo.coder@gmail.com>
2024-09-13 15:41:43 -0300

    cleaner solution

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,21 +1,16 @@
 pub fn verse(n: u32) -> String {
-    if n == 0 {
-        return String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.");
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."),
+        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall."),
+        2 => String::from("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall."),
+        _ => format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, {1} bottles of beer on the wall.", n, n -1)
     }
-    if n == 1 {
-        return format!{"1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall."}
-    }
-    if n == 2 {
-        return format!{"2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall."};
-    }
-    format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, {1} bottles of beer on the wall.", n, n -1)
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    let mut song = String::new();
-    for i in (end..=start).rev() {
-        song.push_str(&verse(i));
-        song.push_str("\n\n");
-    }
-    song
+    (end..=start)
+        .rev()
+        .map(|n| verse(n))
+        .collect::<Vec<String>>()
+        .join("\n\n")
 }
